aec42cb24e5397ba870e86f6e8e2bcfc588d6bb0
Nataliia Korobkina <n.korobkina.888@gmail.com>
2019-02-09 00:53:43 -0600

    Add modal popups

--- Portfolio/Scripts/JSEx/JSExercises.js
+++ Portfolio/Scripts/JSEx/JSExercises.js
@@ -2,32 +2,42 @@
     //ex.1
     $("#numbers-calc").submit(function (e) {
         e.preventDefault();
-        let inp1NumCalc = parseInt($("#input1-numbers").val());
-        let inp2NumCalc = parseInt($("#input2-numbers").val());
-        let inp3NumCalc = parseInt($("#input3-numbers").val());
-        let inp4NumCalc = parseInt($("#input4-numbers").val());
-        let inp5NumCalc = parseInt($("#input5-numbers").val());
+        let arrInputs = [];
+        arrInputs[0] = parseInt($("#input1-numbers").val());
+        arrInputs[1] = parseInt($("#input2-numbers").val());
+        arrInputs[2] = parseInt($("#input3-numbers").val());
+        arrInputs[3] = parseInt($("#input4-numbers").val());
+        arrInputs[4] = parseInt($("#input5-numbers").val());
 
-        if (!inp1NumCalc || !inp2NumCalc || !inp3NumCalc || !inp4NumCalc || !inp5NumCalc) {
+        if (!arrInputs[0] || !arrInputs[1] || !arrInputs[2] || !arrInputs[3] || !arrInputs[4]) {
             $("#rslt-least").text("All fields should be filled, just numbers are accepted!");
+            $("#rslt-greatest").text("");
+            $("#rslt-sum").text("");
+            $("#rslt-mean").text("");
+            $("#rslt-product").text("");
         } else {
-            let sum = inp1NumCalc + inp2NumCalc + inp3NumCalc + inp4NumCalc + inp5NumCalc;
+            let sum = arrInputs.reduce(function (total, num) {
+                return total + num;
+            });
             //least number
-            $("#rslt-least").text("Least number is: " + Math.min(inp1NumCalc, inp2NumCalc, inp3NumCalc, inp4NumCalc, inp5NumCalc));
+            $("#rslt-least").text("Least number is: " + Math.min(...arrInputs));
             //greatest number
-            $("#rslt-greatest").text("Greatest number is: " + Math.max(inp1NumCalc, inp2NumCalc, inp3NumCalc, inp4NumCalc, inp5NumCalc));
+            $("#rslt-greatest").text("Greatest number is: " + Math.max(...arrInputs));
             //the sum of the numbers
             $("#rslt-sum").text("The sum of the numbers is: " + sum);
             //the mean of the numbers
             $("#rslt-mean").text("The mean of the numbers is: " + sum / 5);
             //the product of the numbers
-            $("#rslt-product").text("The product of the numbers is: " + (inp1NumCalc * inp2NumCalc * inp3NumCalc * inp4NumCalc * inp5NumCalc));
+            $("#rslt-product").text("The product of the numbers is: " + (arrInputs.reduce(function (total, num) {
+                return total * num
+            })));
         }
-    })
+    });
+    //Modal window ex.1
     $("#btn-ex1-code").click(function () {
         $("#ex1-code").toggle();
-        $(this).text() == "Show code" ? $(this).text("Hide code") : $(this).text("Show code"); 
-    })
+        $(this).text() == "Show code" ? $(this).text("Hide code") : $(this).text("Show code");
+    });
     //ex.2
     $("#factorial-calc").submit(function (e) {
         e.preventDefault();
@@ -40,18 +50,19 @@
             }
             $("#rslt-factorial").text(`Factorial is: ${result}`);
         }
-    })
+    });
+    //Modal window ex.2
     $("#btn-ex2-code").click(function () {
         $("#ex2-code").toggle();
         $(this).text() == "Show code" ? $(this).text("Hide code") : $(this).text("Show code");
-    })
+    });
     //ex3
     $("#fb-calc").submit(function (e) {
         e.preventDefault();
         let input1Multiples = parseInt($("#inp1-fb").val());
         let input2Multiples = parseInt($("#inp2-fb").val());
         if (!input1Multiples || !input2Multiples) {
-            alert("All fields should be filled, just numbers are accepted!");
+            $("#rslt-fb").text("All fields should be filled, just numbers are accepted!");
         } else {
             let allNumbers = [];
             for (i = 1; i <= 100; i++) {
@@ -68,18 +79,19 @@
             let newString = allNumbers.join(', ');
             $("#rslt-fb").text(newString);
         }
-    })
+    });
+    //Modal window ex.3
     $("#btn-ex3-code").click(function () {
         $("#ex3-code").toggle();
         $(this).text() == "Show code" ? $(this).text("Hide code") : $(this).text("Show code");
-    })
+    });
     //ex4
     $("#polidrome-calc").submit(function (e) {
         e.preventDefault();
         let initialString = $("#inp-polidrome").val();
         let reverseString = initialString.split("").reverse().join("");
-        if (typeof initialString !== "string") {
-            alert("Just a word is accepted!");
+        if (!isNaN(initialString) || initialString === "") {
+            $("#rslt-polidrome").text("The field should be filled. Just a word is accepted!");
         } else {
             if (initialString === reverseString) {
                 $("#rslt-polidrome").text(`Your word is a Palindrome. Reverse string is "${reverseString}"`);
@@ -87,10 +99,16 @@
                 $("#rslt-polidrome").text(`Your word is NOT a Palindrome. Reverse string is "${reverseString}"`);
             }
         }
-
-    })
+    });
+    //Modal window ex.4
     $("#btn-ex4-code").click(function () {
         $("#ex4-code").toggle();
         $(this).text() == "Show code" ? $(this).text("Hide code") : $(this).text("Show code");
-    })
-})       
+    });
+    //modal popups on hover
+    $('[data-toggle="popover"]').popover({
+        placement: 'top',
+        trigger: 'hover',
+        container: 'body'
+    });
+});       
